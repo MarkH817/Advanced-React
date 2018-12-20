5d89eaa90e103fb337d1959937ccc1dcc9b3d8a8
Mark Hernandez <mark@lion-byte.com>
2018-12-19 18:00:12 -0600

    Module 2 - End

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,18 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Header from './Header'
 import Meta from './Meta'
+import Theme from './Theme'
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 const Page = props => {
   const { children } = props
 
   return (
-    <div>
-      <Meta />
-      <Header />
+    <Theme>
+      <StyledPage>
+        <Meta />
+        <Header />
 
-      {children}
-    </div>
+        <Inner>{children}</Inner>
+      </StyledPage>
+    </Theme>
   )
 }
 
